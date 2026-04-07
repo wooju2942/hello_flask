c4ba206b54ed38f7a9035125d7429a1b8316f371
Kim WooJu <coolwjk7759@gmail.com>
2026-04-07 13:52:17 +0900

    feat: 리스트와 for문으로 화면 자동 생성 구현

---
+++ README.md
@@ -0,0 +1,22 @@
+# HELLO_FLASK
+
+## 프로젝트 소개
+Flask와 Jinja2를 사용해서
+파이썬 데이터를 HTML 화면에 출력하는 연습을 하는 프로젝트입니다.
+
+## 지난 시간에 배운 내용
+- Flask 기본 실행
+- templates 폴더 사용
+- index.html 만들기
+- render_template()로 HTML 연결하기
+- 파이썬 딕셔너리 데이터를 HTML에 출력하기
+
+## 이번 시간에 배울 내용
+- 리스트 데이터 만들기
+- for 문으로 화면 자동 생성하기
+- if 문으로 화면 다르게 출력하기
+
+## 프로젝트 파일 설명
+- `app.py` : Flask 서버를 실행하는 파일
+- `templates/index.html` : 화면을 만드는 HTML 파일
+- `README.md` : 프로젝트 설명 문서
--- app.py
+++ app.py
@@ -4,20 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-		# 여기에 내 데이터를 만든다!
-    my_profile = {
-        "name": "김우주",
-        "age": 19,
-        "school": "종로산업정보학교",
-        "hobby": "헬스",
-        "email": "wooju2942@naver.com",
-        "phon": "010-7759-2942",
-        "dream": "데이터 사이언스",
-        "favorite_food": "고기"
-    }
-
-    # 이 부분이 바뀌었다!
-    return render_template('index.html', data=my_profile)
+    foods = ["치킨", "피자", "햄버거","떡볶이", "라면", "초밥","김밥"]
+    return render_template('index.html', data=foods)
 
 if __name__ == '__main__':
     app.run(debug=True)
